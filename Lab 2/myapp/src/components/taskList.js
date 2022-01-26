b9c2514a52a32d2c78bd5ceb0164932edd49a352
Rafsan <rafsan.quayes@gmail.com>
2022-01-26 09:18:48 +0600

    Added some stuff

---
+++ Lab 2/myapp/src/components/taskList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useState } from "react/cjs/react.development";
+import { Task } from "./task";
+
+export const TaskList = ({ data }) => {
+  const [taskList, setTaskList] = useState(data);
+
+  return (
+    <div id="tasklist">
+      <ul id="tasklist">
+        {taskList?.map((task, index) => {
+          return (
+            <li key={task.id}>
+              <Task index={index} task={task}/>
+            </li>
+          );
+        })}
+      </ul>
+      <AddTask taskList={taskList} setTaskList={setTaskList} />
+    </div>
+  );
+};
